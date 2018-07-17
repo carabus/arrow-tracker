f4e524feee28565bdaac1c6da01cd2815969b57d
Svetlana Raeva <svetlana.myth@gmail.com>
2018-07-17 13:10:26 -0700

    config file

---
+++ config.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.DATABASE_URL =
+  process.env.DATABASE_URL || "mongodb://localhost/archery-tracker";
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-archery-tracker";
+exports.PORT = process.env.PORT || 8080;
+exports.CLIENT_ORIGIN =
+  process.env.CLIENT_ORIGIN || "https://frozen-springs-98416.herokuapp.com";
