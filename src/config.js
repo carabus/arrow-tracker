c60ff18b42ae9a7de230abbe7d1db1bbca96ef5f
Svetlana Raeva <svetlana.myth@gmail.com>
2018-08-07 13:53:13 -0700

    api url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://secret-brushlands-10612.herokuapp.com/api";
