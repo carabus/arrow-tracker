f4afc55463366585201466e09aac0d4e2a9ff29b
Svetlana Raeva <svetlana.myth@gmail.com>
2018-10-27 12:58:38 -0700

    fixed incorrect link to backend

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://secret-brushlands-10612.herokuapp.com/api";
+  'https://frozen-mesa-46017.herokuapp.com/api';
 
 export const DEMO_USERNAME =
-  process.env.REACT_APP_DEMO_USERNAME || "Eldridge.Purdy";
+  process.env.REACT_APP_DEMO_USERNAME || 'Eldridge.Purdy';
 
 export const DEMO_PASSWORD =
-  process.env.REACT_APP_DEMO_PASSWORD || "password1234";
+  process.env.REACT_APP_DEMO_PASSWORD || 'password1234';
